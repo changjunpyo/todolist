4aa31c538bbeef3b32267a8bcde7959ad431dfc0
changjunpyo <changjunpyo@naver.com>
2019-07-01 17:17:35 +0900

    .env file add

--- todo-list/src/containers/TodosContainer.js
+++ todo-list/src/containers/TodosContainer.js
@@ -60,7 +60,6 @@ class TodosContainer extends Component {
 export default connect(
     // state 를 비구조화 할당 해주었습니다
     ({ todo }) => ({
-        // immutable 을 사용하니, 값을 조회 할 때엔느 .get 을 사용해주어야하죠.
         todos: todo.get('todos')
     }),
     (dispatch) => ({
